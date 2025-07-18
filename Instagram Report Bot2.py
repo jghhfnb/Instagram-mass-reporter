86d060f6c501651d23607c2e288750b304b4d8ef
jghhfnb <mrspraveen143@gmail.com>
2025-07-18 17:49:17 +0530

    Update Instagram Report Bot2.py

--- Instagram Report Bot2.py
+++ Instagram Report Bot2.py
@@ -23,7 +23,7 @@ username = args.username
 acc_file = args.file
 
 if username == "" :
-	username = input("Username: ")
+	username = input("__anf_toji_: ")
 
 a = open(acc_file, "r").readlines()
 file = [s.rstrip()for s in a]
